37b605b2c423491a95ad2da55b26a69f86d316c5
root <vincegrixc@gmail.com>
2017-12-05 18:08:24 +0100

    i dont want to commit but git is forcing me :(

--- src/de/htwg/se/kakuro/model/Sum.scala
+++ src/de/htwg/se/kakuro/model/Sum.scala
@@ -3,7 +3,7 @@ package de.htwg.se.kakuro.model
 import scala.math.sqrt
 import scala.util.Random
 
-
+/*
 case class Sum(cell: Cell) {
   def this(size: Int) = this(new Vector[Cell](size, WhiteCell(0)))
   def infoCell: BlackCell
@@ -11,3 +11,4 @@ case class Sum(cell: Cell) {
   def value: Int
   def 
 }
+*/
