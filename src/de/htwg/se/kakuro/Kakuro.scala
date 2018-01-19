e4471820ae275f1045e535d14eba60e9708abb92
vincent <vigrimme@htwg-konstanz.de>
2018-01-19 18:16:56 +0100

    Merge branch 'dev'
    # Conflicts:
    #	README.md

--- src/de/htwg/se/kakuro/Kakuro.scala
+++ src/de/htwg/se/kakuro/Kakuro.scala
@@ -14,7 +14,7 @@ object Kakuro {
     val controller = new Controller(new Field(defaultsize))
     //var field = Field(8, 8)
     val tui = new Tui(controller)
-    val gui = new SwingGui(controller)
+    //val gui = new SwingGui(controller)
     var input: String = ""
     do {
       tui.printTui()
